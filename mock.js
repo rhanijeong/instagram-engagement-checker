3fbd06eab7722b48fc6e7a537a22940a7eba597f
rhanijeong <onicau2017@gmail.com>
2026-01-15 19:08:48 +0900

    add mock data

---
+++ mock.js
@@ -0,0 +1,10 @@
+const mockPosts = [
+  { views: 12400, likes: 820, comments: 43 },
+  { views: 9800, likes: 610, comments: 31 },
+  { views: 15600, likes: 1100, comments: 72 },
+  { views: 8700, likes: 540, comments: 26 },
+  { views: 13400, likes: 920, comments: 48 },
+  { views: 10200, likes: 670, comments: 35 },
+  { views: 9100, likes: 590, comments: 29 },
+  { views: 14800, likes: 1050, comments: 64 },
+];
